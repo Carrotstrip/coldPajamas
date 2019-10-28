e94bc7b4967d2dc078b2172e7e54122d50fc9ceb
jacobfed <jacobfed@umich.edu>
2019-10-27 22:54:54 -0400

    Added start text and bobbing

---
+++ XstreamFishing/Assets/Scripts/GameManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public Text startupText;
+    private float timer;
+    private bool timerRunning;
+    // Start is called before the first frame update
+    void Start()
+    {
+        startupText.text = "It's a nice day to be out fishing, \nfeel free to come on down to the pro shop for supplies\nHeck I'll even throw in some free advice";
+        timerRunning = false;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timerRunning){
+            timer += Time.deltaTime;
+            if (timer >= 4.0f){
+                startupText.text = "";
+            }
+            if (timer >= 10.0f){
+                startupText.text = "";
+            }
+
+        }
+        if (!timerRunning && (Input.GetAxis ("Horizontal") != 0 || Input.GetAxis ("Vertical") != 0)){
+            timer = 0.0f;
+            timerRunning = true;
+        }
+        
+    }
+}
---
+++ XstreamFishing/Assets/Scripts/ShipRocker.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using UnityEngine;
+ 
+public class ShipRocker: MonoBehaviour
+{
+    //updown movement from
+    //https://forum.unity.com/threads/trying-to-simulate-a-ships-rocking-motion-in-the-ocean.465557/
+ 
+    float originalY;
+    public float speed = 2.0f;
+    public float bobbingMultiplier = .5f;
+    public float rollMultiplier = .4f;
+    private Quaternion startRotation;
+ 
+    void Start()
+    {
+        originalY = this.transform.position.y;
+        // startRotation = transform.rotation;
+    }
+ 
+    void Update()
+    {
+        BobUpAndDown();
+        // RollFrontToBack();
+        // RollSideToSide();
+    }
+ 
+    void BobUpAndDown( )
+    {
+        transform.position = new Vector3(this.transform.position.x,
+                                         originalY + ((float)Math.Sin(Time.time*speed) * bobbingMultiplier),
+                                         this.transform.position.z);
+    } 
+    // void RollSideToSide( )
+    // {
+    //     float f = Mathf.Sin( Time.time * rollMultiplier ) * 10f;
+    //     transform.rotation = startRotation * Quaternion.AngleAxis( f, Vector3.forward );
+    // }
+ 
+    // void RollFrontToBack()
+    // {
+    //     float f = Mathf.Sin(Time.time * rollMultiplier) * 2f;
+    //     transform.rotation = startRotation * Quaternion.AngleAxis(f, Vector3.left);
+    // }
+}
+ 
