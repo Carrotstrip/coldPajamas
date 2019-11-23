c8a262b4ddd4dc475bd3f6e5575b4b93012c27fe
wolfaust <wolfaust@umich.edu>
2019-11-23 15:26:05 -0500

    fish json

---
+++ XstreamFishing/Assets/Scripts/Fish.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Fish {
+
+  public int index;
+  public string species;
+  public float catchCap;
+  public float releaseCap;
+  public float minSpeed;
+  public float maxSpeed;
+  public int value;
+  public Image icon;
+
+  
+
+}
