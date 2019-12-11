35c1a8cc944c759a10b35ceba499edf923059cea
wolfaust <wolfaust@umich.edu>
2019-12-11 16:11:01 -0500

    nar

--- XstreamFishing/Assets/Scripts/AudioManager.cs
+++ XstreamFishing/Assets/Scripts/AudioManager.cs
@@ -8,10 +8,10 @@ using System.Collections.Generic;
         public static AudioManager instance = null;
 
         public AudioSource efxSource;
-        public AudioSource p0Source;
-        public AudioSource p1Source;
-        public AudioSource p2Source;
-        public AudioSource p3Source;
+        // public AudioSource p0Source;
+        // public AudioSource p1Source;
+        // public AudioSource p2Source;
+        // public AudioSource p3Source;
 
         void Awake ()
         {
@@ -44,49 +44,51 @@ using System.Collections.Generic;
         }
         
         public void PlaySoundEffect(AudioClip clip, int playerIndex){
-            switch (playerIndex){
-                case 0:
-                    p0Source.clip = clip;
-                    p0Source.Play();
-                    break;
-                case 1:
-                    p1Source.clip = clip;
-                    p1Source.Play();
-                    break;
-                case 2:
-                    p2Source.clip = clip;
-                    p2Source.Play();
-                    break;
-                case 3:
-                    p3Source.clip = clip;
-                    p3Source.Play();
-                    break;
-                default:
-                    efxSource.clip = clip;
-                    efxSource.PlayOneShot(clip,1.0f);
-                    break;
+            this.Play(clip);
+            // switch (playerIndex){
+            //     case 0:
+            //         p0Source.clip = clip;
+            //         p0Source.Play();
+            //         break;
+            //     case 1:
+            //         p1Source.clip = clip;
+            //         p1Source.Play();
+            //         break;
+            //     case 2:
+            //         p2Source.clip = clip;
+            //         p2Source.Play();
+            //         break;
+            //     case 3:
+            //         p3Source.clip = clip;
+            //         p3Source.Play();
+            //         break;
+            //     default:
+            //         efxSource.clip = clip;
+            //         efxSource.PlayOneShot(clip,1.0f);
+            //         break;
 
-            }
+            //}
         }
         public void Stop(int playerIndex){
-            switch (playerIndex){
-                case 0:
-                    p0Source.Stop();
-                    break;
-                case 1:
-                    p1Source.Stop();
-                    break;
-                case 2:
-                    p2Source.Stop();
-                    break;
-                case 3:
-                    p3Source.Stop();
-                    break;
-                default:
-                    efxSource.Stop();
-                    break;
+            efxSource.Stop();
+            // switch (playerIndex){
+            //     case 0:
+            //         p0Source.Stop();
+            //         break;
+            //     case 1:
+            //         p1Source.Stop();
+            //         break;
+            //     case 2:
+            //         p2Source.Stop();
+            //         break;
+            //     case 3:
+            //         p3Source.Stop();
+            //         break;
+            //     default:
+            //         efxSource.Stop();
+            //         break;
 
-            }
+            // }
         }
 
     }
