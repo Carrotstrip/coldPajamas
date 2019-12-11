e5114e5714470803d2bfe279a5fb4402cef2ca48
wolfaust <wolfaust@umich.edu>
2019-12-11 14:41:41 -0500

    change texts

--- XstreamFishing/Assets/Scripts/WinningText.cs
+++ XstreamFishing/Assets/Scripts/WinningText.cs
@@ -10,8 +10,8 @@ public class WinningText : MonoBehaviour
     void Start()
     {
         t = GetComponent<Text>();
-        t.text = "I've been trying to catch that beast for 50 years... I don't know how to thank you Player " 
-                  + GameManager.winningPlayer + ". How about I make you some of my famous cannoli?";
+        t.text = "I've been trying to catch that beast for 50 years... \nI don't know how to thank you Player " 
+                  + GameManager.winningPlayer + ". \nHow about I make you some of my famous cannoli?";
     }
 
     // Update is called once per frame
